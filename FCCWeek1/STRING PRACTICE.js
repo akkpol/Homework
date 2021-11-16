9047a7880cdbaed63074b4abfecba80f6330dc5d
akkpol <75881756+akkpol@users.noreply.github.com>
2021-11-16 09:40:34 +0700

    Add files via upload

---
+++ FCCWeek1/STRING PRACTICE.js
@@ -0,0 +1,72 @@
+// PartA-พื้นฐานการประกาศตัวแปรและกำหนดค่า (Declaration and Initialization)
+
+// 1.Declare String Variables
+// var myFirstName = "Natchaya";
+// var myLastName = "Boonyaleepan";
+
+// 2.Escaping Literal Quotes in StringsPassed
+// var myStr = "I am a \"double quoted\" string inside \"double quotes\".";
+
+// 3.Quoting Strings with Single Quotes
+// var myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
+
+// 4.Escape Sequences in Strings
+// var myStr ='FirstLine\n\t\\SecondLine\nThirdLine'; // Change this line
+
+///////////////////////////////////////////////////////////////////////
+// PartB-การเชื่อมข้อความ( String Concatenate )
+
+// 1.Concatenating Strings with Plus Operator
+// var myStr = 'This is the start. ' + 'This is the end.';
+
+// 2.Concatenating Strings with the Plus Equals Operator
+// var myStr = 'This is the first sentence. ';
+// myStr += 'This is the second sentence.';
+
+// 3.Constructing Strings with Variables
+// var myName = 'Minny';
+// var myStr = 'My name is ' + myName + 'and I am well!';
+
+// 4.Appending Variables to Strings
+// var someAdjective = 'OMG';
+// var myStr = "Learning to code is ";
+// myStr += someAdjective;
+
+///////////////////////////////////////////////////////////////////////
+// PartC-การเข้าถึงและแก้ไขแต่ละตัวอักษร( Access and Mutate )
+
+// 1.Find the Length of a StringPassed
+// var lastNameLength = 0;
+// var lastName = "Lovelace";
+// lastNameLength = lastName.length;
+
+// 2.Use Bracket Notation to Find the First Character in a String
+// var firstLetterOfLastName = "";
+// var lastName = "Lovelace";
+// firstLetterOfLastName = lastName[0];
+
+// 3.Use Bracket Notation to Find the Nth Character in a String
+// var lastName = "Lovelace";
+// var thirdLetterOfLastName = lastName[2];
+
+// 4.Use Bracket Notation to Find the Last Character in a String
+// var lastName = "Lovelace";
+// var lastLetterOfLastName = lastName[lastName.length - 1];
+
+// 5.Use Bracket Notation to Find the Nth-to-Last Character in a String
+// var lastName = "Lovelace";
+// var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
+// 6.Understand String Immutability
+// var myStr = "Jello World";
+// var myStr = "Hello World";
+
+///////////////////////////////////////////////////////////////////////
+// UseCase
+
+// 1.Word Blanks
+// var myNoun = "dog";
+// var myAdjective = "big";
+// var myVerb = "ran";
+// var myAdverb = "quickly";
+// var wordBlanks = myNoun + " " + myAdjective + " " + myVerb + " " + myAdverb;
---
+++ FCCWeek1/Variable.js
@@ -0,0 +1,39 @@
+//1.Comment Your JavaScript Code
+//Hello
+/*Hello it's me*/
+
+// 2.Declare JavaScript Variables
+// var ourName;
+// var myName;
+
+// 3.Storing Values with the Assignment Operator
+// var a;
+// var myVar;
+// myVar = 5;
+// a = 7;
+
+// 4.Assigning the Value of One Variable to Another
+// var a;
+// a = 7;
+// var b;
+// b = a;
+
+// 5.Initializing Variables with the Assignment Operator
+// var a = 9;
+
+// 6.Understanding Uninitialized Variables
+// var a = 5;
+// var b = 10;
+// var c = "I am a";
+// a = a + 1;
+// b = b + 5;
+// c = c + " String!";
+
+// 7.Understanding Case Sensitivity in Variables
+// var studlyCapVar;
+// var properCamelCase;
+
+// var titleCaseOver;
+// studlyCapVar = 10;
+// properCamelCase = "A String";
+// titleCaseOver = 9000;
